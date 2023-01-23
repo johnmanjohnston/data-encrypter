212e6767765765f6a898369cd7c6b37cb1954dcd
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-23 18:17:20 +0000

    Created necesarry header and C++ files

---
+++ dataencrypter/Common.h
@@ -0,0 +1,3 @@
+#include <string>
+void Encrypt(std::string raw, std::string key);
+void Decrypt(std::string encRaw, std::string key);
---
+++ dataencrypter/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "Common.h"
+
+int main() {
+    std::cout << "Main.cpp working";
+    
+    return 0;
+}
