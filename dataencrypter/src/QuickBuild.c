b4230bb090d0a4e68194125a9aa41a8f338ff208
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-24 18:28:07 +0000

    Main logic w/ temp fixed filenames

--- dataencrypter/src/QuickBuild.c
+++ dataencrypter/src/QuickBuild.c
@@ -1,1 +1,10 @@
-// Builds and runs .cpp file on prompt
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Fixed filenames (temporarily)
+    system("g++ Main.cpp");
+    system("./a.out");
+
+    return 0;
+}
