f57c7541581b935f46e17c7ab3c8a36cc2cf966d
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-26 16:59:35 +0000

    Main.cpp accepts relative path to target dir

--- dataencrypter/src/Main.cpp
+++ dataencrypter/src/Main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <iostream>
 #include "../headers/Common.h"
 
+using namespace std;
+
+// Define globals
+std::string RelativeDir;
 int main() {
     std::cout << "Main.cpp working";
+    std::cin >> RelativeDir;
+
+    for (const auto & entry : filesystem::directory_iterator(RelativeDir))
+        std::cout << entry.path() << std::endl;
     
     return 0;
 }
