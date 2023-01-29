2f5fb1fbeb05975d3e938b9a57ad73b85eb7bcc8
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-29 12:19:29 +0000

    Refactors

--- dataencrypter/src/Utils.c
+++ dataencrypter/src/Utils.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 #include "../headers/Utils.h"
 
-const char* VALIDATOR_PHRASE = "This file is required to validate your key! Don't tamper with this file.";
+const char* ValidationPhrase = "This file is required to validate your key! Don't tamper with this file.";
 
 bool ValidateKey(const char* validatorFileData) {
-    int matches = strcmp(validatorFileData, VALIDATOR_PHRASE) == 0;
+    int matches = strcmp(validatorFileData, ValidationPhrase) == 0;
     return matches == 1;
 }
