1c50bb34dc80f2d6c7b83f481a1be17810e99941
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2023-01-31 17:01:40 +0000

    Removed commented code

--- dataencrypter/src/Crypt.cpp
+++ dataencrypter/src/Crypt.cpp
@@ -4,17 +4,6 @@
 #include "../headers/Crypt.hpp"
 #include "../headers/Utilscp.hpp"
 
-// bool StrEndsWith(const std::string& orgstr, const std::string& suffix) {
-//     size_t lStr = orgstr.length();
-//     size_t lSuffix = suffix.length();
-
-//     // if suffix is larger than string we desire to compare, it's not possible to return true  
-//     // as the orginal string can't end with a string larger than itself
-//     if (lSuffix > lStr) return false; 
-
-//     return orgstr.compare(lStr - lSuffix, lSuffix, suffix) == 0;
-// }
-
 std::string CryptValidationFilename = ".crypt-validation.txt";
 
 void CryptHandler::Decrypt(
--- dataencrypter/src/Main.cpp
+++ dataencrypter/src/Main.cpp
@@ -10,14 +10,6 @@
 std::string RelativePath;
 std::string CryptionKey;
 
-inline bool strEndsWith(std::string const &orgStr, std::string const &suffix) {
-    // If suffix size > original string, it isn't possible to have the orginal string contain the suffix
-    if (suffix.size() > orgStr.size()) { return false; }
-
-    bool ends = std::equal(suffix.rbegin(), suffix.rend(), orgStr.rbegin());
-    return ends;
-}
-
 int main() {
     std::cout << "Enter the path of the files of the directory you want to crypt: ";
     std::cin >> RelativePath;
