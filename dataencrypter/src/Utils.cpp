436efc658cb1624fd32fa61c07968bace483eb5a
daviesmiths <122976792+daviesmiths@users.noreply.github.com>
2023-02-02 12:32:57 +0400

    Refactors

--- dataencrypter/src/Utils.cpp
+++ dataencrypter/src/Utils.cpp
@@ -2,12 +2,8 @@
 #include <string>
 
 bool StrEndsWith(const std::string& orgstr, const std::string& suffix) {
-    size_t lStr = orgstr.length();
-    size_t lSuffix = suffix.length();
-
     // if suffix is larger than string we desire to compare, it's not possible to return true  
     // as the orginal string can't end with a string larger than itself
-    if (lSuffix > lStr) return false; 
-
+    if (suffix.length() > orgstr.length()) return false; 
     return orgstr.compare(lStr - lSuffix, lSuffix, suffix) == 0;
 }
